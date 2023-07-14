add436b1d17f4302d77e3b8b143ca50e11abd0d5
YEYEVERNON <vcyvernon@gmail.com>
2023-07-14 15:21:39 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+int singleDigBaseTen;
+for (singleDigBaseTen=0; singleDigBaseTen<10; singleDigBaseTen++)
+{
+	putchar((singleDigBaseTen%10) + '0');
+}
+printf("\n");
+
+return(0);
+
+{
