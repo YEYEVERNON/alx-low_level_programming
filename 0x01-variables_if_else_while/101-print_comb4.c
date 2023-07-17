e9a0ec0ef134341af3bff4389794efe5b30306b5
YEYEVERNON <vcyvernon@gmail.com>
2023-07-17 14:14:16 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n=48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
-		for (m = 49; m <58; m++)
+		for (m = 49; m < 58; m++)
 		{
 			for (l = 50; l < 58; l++)
 			{
@@ -20,7 +20,7 @@ int main(void)
 				putchar(n);
 				putchar(m);
 				putchar(l);
-				if (n != 55 || m !=56)
+				if (n ! = 55 || m ! = 56)
 				{
 					putchar(',');
 					putchar(' ');
