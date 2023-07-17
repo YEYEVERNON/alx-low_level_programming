2f185e38d6291ef6fe1ea9fb44997ff8c3df204e
YEYEVERNON <vcyvernon@gmail.com>
2023-07-17 13:21:55 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-    	int i;
-
-    	for (i = 0 ; i < 10 ; i++)
-    	{
-            	putchar(i + '0');
+	int i;
+	
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
             	putchar(',');
             	putchar(' ');
 
     	}
-    	return (0);
+	return (0);
 
 }
