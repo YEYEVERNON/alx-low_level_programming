ffd08e806bfc829fd89d8d601ef123bf0b034e9e
YEYEVERNON <vcyvernon@gmail.com>
2023-07-19 08:19:42 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * jack_baver - Prints every minute of the day of Jack Baver
+ * jack_bauer - Prints every minute of the day of Jack Baver
  *
  * starting from 00.00 to 23.59
  *
  */
-void jack_baver(void)
+void jack_bauer(void)
 {
 	int i, j;
 
