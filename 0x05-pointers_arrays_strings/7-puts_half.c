2500ca2b3f02286a7bf8191fedf5cff3fb9a9e12
YEYEVERNON <vcyvernon@gmail.com>
2023-07-24 14:38:46 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: string
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\n')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
