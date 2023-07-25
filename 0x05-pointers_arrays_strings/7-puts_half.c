cfb155a17d35214289bcaa27aeaab225785cd68a
YEYEVERNON <vcyvernon@gmail.com>
2023-07-25 07:42:18 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,17 +7,27 @@
  */
 void puts2(char *str)
 {
-	int i;
 	int j = 0;
+	int k;
 
-	while (str[j] != '\')
+	while (str[j] != '\0')
 	{
 		j++;
 	}
-
-	for (i = 0; i < j; i +=2)
+	if (j % 2 == 1)
 	{
-		_putchar(str[i]);
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
+
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
 	}
 	_putchar('\n');
 }
+
