560001a6e3a55d7f515910f9b4a1f23b374b13e0
YEYEVERNON <vcyvernon@gmail.com>
2023-07-28 12:35:52 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,21 +10,19 @@
 char *leet(char *str)
 {
 	int i, j;
-	char c[] = "aAeEoOtTlL";
-	char d[] = "4433007711";
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-	for (i = 0; str[i] != '\0'; j++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; c[j] != '\0'; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (str[i] == c[j])
+
+			if (n[i] == s1[j])
 			{
-				if (str[i] == c[j])
-				{
-					str[i] = d[j];
-				}
+				n[i] = s2[j];
 			}
 		}
-		return (str);
 	}
+	return (n);
 }
