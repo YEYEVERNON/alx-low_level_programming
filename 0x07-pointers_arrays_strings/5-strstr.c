0a46988b6569f91d5f6f86f564391e08806767af
YEYEVERNON <vcyvernon@gmail.com>
2023-07-31 14:39:39 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,18 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_chessboard - Entry point
  * @a: array
  * Return: Always 0 (success)
  */
-void print_chessboard(char (*a)[8])
+char *_strstr(char *haystack, char *needle)
 {
-	int i;
-	int j;
-
-	for (i = 0; i < 8; j++)
+	for (; *haystack != "\0"; haystack++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		_putchar('\n');
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystack);
 	}
+
+	return (NULL);
 }
